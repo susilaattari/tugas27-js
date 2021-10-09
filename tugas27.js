55c63c76ac23a2adb826c5896114d2d71496de48
susila tropika <64695610+susilaattari@users.noreply.github.com>
2021-10-10 03:58:29 +0700

    Tugas 27 javascript niomic

---
+++ tugas27.js
@@ -0,0 +1,12 @@
+let a = 'No ';
+let b = 'Rumah Saya ';
+let c =30;
+let d = 'A';
+let hasilKalimat = a+b+c+d;
+
+let kalimat = 'Jalan pangeran Tirtayasa No '
+let number = 70
+let hasil = kalimat+number
+
+console.log(hasilKalimat);
+console.log(hasil)
